fd8ba4c62865dd3edbfb7c3fdf58e51b7afa0f82
kimmttrung <160985982+kimmttrung@users.noreply.github.com>
2025-09-17 15:52:59 +0700

    setup base project

---
+++ 01-backend/src/server.js
@@ -0,0 +1,11 @@
+import express from 'express';
+import { ENV } from "./config/env.js";
+
+const app = express();
+
+
+app.get("/", (req, res) => {
+    res.send("hello word");
+})
+
+app.listen(ENV.PORT, () => console.log("Sever started on port", ENV.PORT));
