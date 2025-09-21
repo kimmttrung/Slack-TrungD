80b2ecdfb27ce90bc0bdc4bb7e483535bfb6c454
kimmttrung <160985982+kimmttrung@users.noreply.github.com>
2025-09-21 16:54:00 +0700

    sentry and complete api

--- 01-backend/src/server.js
+++ 01-backend/src/server.js
@@ -1,4 +1,4 @@
-import "../instrument.js";
+import "../instrument.mjs";
 import express from 'express';
 import { ENV } from "./config/env.js";
 import { connectDB } from './config/db.js';
@@ -6,13 +6,16 @@ import { clerkMiddleware } from "@clerk/express";
 import { inngest, functions } from './config/inngest.js';
 import { serve } from "inngest/express";
 import chatRoutes from './routes/chat.route.js';
+import * as Sentry from "@sentry/node";
 
 const app = express();
 
 app.use(express.json());
 app.use(clerkMiddleware());
-// app.get("/sentry-debug")
 
+app.get("/debug-sentry", (req, res) => {
+    throw new Error("My first Sentry error!");
+});
 app.get("/", (req, res) => {
     res.send("hello word");
 })
